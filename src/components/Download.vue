1fe9f5fdfd4bc4411c9f726f3c105b48748ecb6c
r.kokubun <r.kokubun@honeycom-b-works.com>
2019-11-16 14:23:21 +0900

    1st commit.

---
+++ src/components/Download.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="2">
+                <v-btn
+                    v-on:click="click"
+                    color="primary"
+                    block
+                >
+                    Download csv
+                </v-btn>
+            </v-col>
+
+            <v-col cols="2">
+                <v-btn
+                    v-on:click="click"
+                    color="primary"
+                    block
+                >
+                    Download image
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    methods: {
+        click () {
+        }
+    }
+}
+</script>
