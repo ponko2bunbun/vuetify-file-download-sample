35909c695eae1c9ea36b5ebf4b15b1a5126e86fc
r.kokubun <r.kokubun@honeycom-b-works.com>
2019-11-18 21:45:07 +0900

    API を叩いてファイルをダウンロード.

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -3,7 +3,7 @@
         <v-row>
             <v-col cols="2">
                 <v-btn
-                    v-on:click="click"
+                    v-on:click="click('sample.csv')"
                     color="primary"
                     block
                 >
@@ -13,7 +13,7 @@
 
             <v-col cols="2">
                 <v-btn
-                    v-on:click="click"
+                    v-on:click="click('sample.png')"
                     color="primary"
                     block
                 >
@@ -25,9 +25,29 @@
 </template>
 
 <script>
+import axios from 'axios'
+import urljoin from 'url-join'
+import { saveAs } from 'file-saver'
+
 export default {
     methods: {
-        click () {
+        async click (fileName) {
+            const response = await axios.get(urljoin('http://localhost:3000/api/download'), {
+                responseType: 'blob',
+                params: {
+                    'file': fileName
+                }
+            }).catch((error) => {
+                console.log(error)
+            })
+
+            if (response && (200 == response.status)) {
+                const contentDisposition = response.headers["content-disposition"];
+                const contentDispositions = contentDisposition.split(';')
+                const fileNames = contentDispositions[contentDispositions.length - 1].split('=')
+                fileName = fileNames[fileNames.length - 1]
+                saveAs(response.data, fileName);
+            }
         }
     }
 }
